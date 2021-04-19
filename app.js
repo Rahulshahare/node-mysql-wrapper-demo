06b43d7ee0588b8c3b56cfcfb9ab7e0af995df48
Rahulshahare <rahulshahare@gmail.com>
2021-04-19 22:00:30 +0530

    mysql-node-wrapper demo

---
+++ app.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'db_localolx'
+});
+
+
+connection.connect( (err)=>{
+	if(err) throw err;
+	console.log('Connected');
+});
+
+connection.query('SELECT * FROM location', (err, rows) =>{
+	if(err) throw err;
+
+	console.log(rows);
+});
